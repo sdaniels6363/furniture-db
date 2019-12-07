ab2bb1b4d543ca7fac2e2dc4bf09c3a4e1ae1667
Toney Higgins <toneyhiggins2@gmail.com>
2019-12-07 12:00:33 -0500

    fixed scraping and pushing into mongo

--- uttermost-scrape.js
+++ uttermost-scrape.js
@@ -27,49 +27,52 @@ app.use(express.static("public"));
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/furniture";
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
-
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex', true);
 // A GET route for scraping the echoJS website
-app.get("/uttermost-scrape", function(req, res) {
+app.get("/uttermost-scrape", function (req, res) {
     // First, we grab the body of the html with axios
-    axios.get("https://www.uttermost.com/Accent-Furniture-Shop-By-Room-Bedroom/").then(function(response) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(response.data);
-  
-      // Now, we grab every h2 within an article tag, and do the following:
-      $("article").each(function(i, element) {
-        // Save an empty result object
-        var result = {};
-  
-        // Add the text and href of every link, and save them as properties of the result object
-        result.img = $(this)
-          .children("a")
-          .children("img")
-          .attr("src");
-        result.url = $(this)
-          .children("a")
-          .attr("href");
+    axios.get("https://www.uttermost.com/Accent-Furniture-Shop-By-Room-Bedroom/").then(function (response) {
+        // Then, we load that into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
 
-          console.log(result);
-  
-        // Create a new Article using the `result` object built from scraping
-        db.Uttermost.create(result)
-          .then(function(dbUttermost) {
-            // View the added result in the console
-            console.log(dbUttermost);
-          })
-          .catch(function(err) {
-            // If an error occurred, log it
-            console.log(err);
-          });
-      });
-  
-      // Send a message to the client
-      res.send("Scrape Complete");
+        // Now, we grab every h2 within an article tag, and do the following:
+        $("article").each(function (i, element) {
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.img = $(this)
+                .children("figure")
+                .children("a")
+                .children("img")
+                .attr("src");
+            result.url = "https://www.uttermost.com/" + $(this)
+                .children("figure")
+                .children("a")
+                .attr("href");
+
+            console.log(`result`, result);
+
+            // Create a new Article using the `result` object built from scraping
+                db.Uttermost.create(result)
+                  .then(function(dbUttermost) {
+                    // View the added result in the console
+                    console.log(dbUttermost);
+                  })
+                  .catch(function(err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                  });
+              });
+
+            // Send a message to the client
+            res.send("Scrape completed");
+        });
     });
-  });
 
 // Start the server
 app.listen(PORT, () => {
     console.log("App running at http://localhost:" + PORT);
 });
+
