9b6c2787c8b99f14d4bb4d8f529e75e82ae634f6
Scott Daniels <sdaniels6363@gmail.com>
2019-12-07 18:12:02 -0500

    updated paths for require db, since the scripts were moved.

--- scraping-scripts/tayKing-scrape.js
+++ scraping-scripts/tayKing-scrape.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 const axios = require("axios");
 
-const db = require("./models");
+const db = require("../models");
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapetest";
 
