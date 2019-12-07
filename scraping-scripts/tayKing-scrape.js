ca55c150ac9bd47b38acfc84614d97531e9c54c1
Scott Daniels <sdaniels6363@gmail.com>
2019-12-07 18:02:30 -0500

    renamed scrape-test to tayKing.scrape.js

---
+++ scraping-scripts/tayKing-scrape.js
@@ -0,0 +1,60 @@
+
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+const db = require("./models");
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapetest";
+
+mongoose.connect(MONGODB_URI);
+
+var searchTK = function (url, category) {
+
+    axios.get(url).then(function (response) {
+
+        var $ = cheerio.load(response.data);
+
+        $("article").each(function (i, element) {
+
+            var result = {};
+
+            let descriptionHtml = $(this)
+                .children(".productText")
+                .html()
+                .trim();
+            let dataSplit = descriptionHtml.split("<br>", 2)
+            result.description = dataSplit[1]
+            result.sku = dataSplit[0]
+
+            result.category = category;
+
+            result.url = "https://taylorking.com/" + $(this)
+                .children("a")
+                .attr("href");
+            result.tearsheet = "https://taylorking.com/" + $(this)
+                .children("a")
+                .attr("href");
+            result.image = "https://taylorking.com/" + $(this)
+                .children()
+                .children()
+                .children("img")
+                .attr("src");
+
+            db.TaylorKing.create(result)
+                .then(function (dbTaylorKing) {
+
+                    console.log(dbTaylorKing);
+                })
+                .catch(function (err) {
+
+                    console.log(err);
+                });
+
+        });
+    });
+
+}
+
+searchTK("https://taylorking.com/category.asp?CID=11", "bedroom");
+searchTK("https://taylorking.com/category3columns.asp?CID=3", "sleepers");
