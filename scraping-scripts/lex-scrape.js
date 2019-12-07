97e29c9fc9bc39b01ea82b827cd9de8689c2d520
Scott Daniels <sdaniels6363@gmail.com>
2019-12-07 18:02:08 -0500

    moved lex-scrape to scraping-scripts dir

---
+++ scraping-scripts/lex-scrape.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+const db = require("./models");
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/furniture";
+
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true });
+
+var searchLex = function (url, category) {
+
+    axios.get(url).then(function (response) {
+
+        var $ = cheerio.load(response.data);
+
+        $("div.product-list-item").each(function (i, element) {
+
+            var result = {};
+
+            result.category = category;
+
+            result.description = $(this)
+                .children()
+                .children()
+                .children()
+                .attr("alt");
+
+            result.url = "https://lexington.com" + $(this)
+                .children()
+                .children()
+                .attr("href");
+
+            result.image = "https://lexington.com" + $(this)
+                .children()
+                .children()
+                .children()
+                .attr("src");
+
+            axios.get(result.url).then(function (response) {
+
+                var $ = cheerio.load(response.data);
+
+                $(".tearsheet").each(function (i, element) {
+                    // console.log(element)
+                    result.tearsheet = $(this)
+                        .attr("href");
+                    // console.log(result.tearsheet)
+
+                    db.Lexington.create(result)
+                        .then(function (dblexington) {
+
+                            console.log(dblexington);
+                        })
+                        .catch(function (err) {
+
+                            console.log(err);
+                        });
+                });
+
+            });
+
+            // console.log(result)
+        });
+    });
+
+}
+
+searchLex("https://lexington.com/beds", "beds");
+searchLex("https://lexington.com/dressers", "dressers");
+searchLex("https://lexington.com/mirrors", "mirrors");
+searchLex("https://lexington.com/chests", "chests");
+searchLex("https://lexington.com/night-stands", "nightstands");
+searchLex("https://lexington.com/benches-ottomans1521", "benches/ottomans");
+
