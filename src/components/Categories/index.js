b9ec74af286f5e57758c392883db2a9226a1375d
Scott Daniels <sdaniels6363@gmail.com>
2019-12-10 23:18:32 -0500

    basic navbar component for item categories no styling

---
+++ src/components/Categories/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Categories() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="/api/category/beds">
+        Beds
+      </a>
+      <a className="navbar-brand" href="/api/category/benches">
+        Benches
+      </a>
+      <a className="navbar-brand" href="/api/category/dressers">
+        Dressers
+      </a>
+      <a className="navbar-brand" href="/api/category/mirrors">
+        Mirrors
+      </a>
+      <a className="navbar-brand" href="/api/category/nightstands">
+        Nightstands
+      </a>
+    </nav>
+  );
+}
+
+export default Categories;
