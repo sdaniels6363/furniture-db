6e45e3dd68461259d8ff45978001f50bc6cedb76
Scott Daniels <sdaniels6363@gmail.com>
2019-12-14 15:45:06 -0500

    category call works

--- controllers/furnitureController.js
+++ controllers/furnitureController.js
@@ -3,8 +3,9 @@ const db = require("../models");
 // Defining methods for the furnitureController
 module.exports = {
   findByCategory: function(req, res) {
+    let category = req.params.category;
     db.Furniture
-      .find(req.params.category)
+      .find({category: category})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
