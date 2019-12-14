cdd0d748e8c9cb5f43b7434a9918b61a1485d8d4
Scott Daniels <sdaniels6363@gmail.com>
2019-12-14 15:04:16 -0500

    reducing to just one route until frontend to backend is good

--- routes/api/furniture.js
+++ routes/api/furniture.js
@@ -4,9 +4,5 @@ const furnitureController = require("../../controllers/furnitureController.js");
 router.route("/:category")
   .get(furnitureController.findByCategory);
 
-router.route("/:category/:vendor")
-  .get(furnitureController.findByVendor);
-
-
 
 module.exports = router;
