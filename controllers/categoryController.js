4a9b23e9022a06e13123ee6993d0faf35a0021fd
Scott Daniels <sdaniels6363@gmail.com>
2019-12-16 18:57:28 -0500

    made navbar dynamic with categories in db

---
+++ controllers/categoryController.js
@@ -0,0 +1,15 @@
+const db = require("../models");
+
+// Defining methods for the categoryController
+module.exports = {
+    findAll: function(req, res) {
+        db.Furniture.distinct("category",(err,data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(data);
+                res.json(data);
+            }
+        })
+    }
+};
