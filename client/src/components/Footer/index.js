ad8ba9494a25b56ef17de667252926fd9dfcdea5
JHaarla <haarladesign@gmail.com>
2019-12-21 11:32:19 -0500

    styling the footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,15 @@
 // import React from './node_modules/react';
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+// import Navbar from 'react-bootstrap/Navbar';
 // import Navbar from './node_modules/react-bootstrap/Navbar'
 // import NavbarBrand from './node_modules/react-bootstrap/NavbarBrand';
-import NavbarBrand from 'react-bootstrap/NavbarBrand';
+// import NavbarBrand from 'react-bootstrap/NavbarBrand';
 import "../../styles/Footer.css";
 
 class AppFooter extends React.Component {
     render() {
         return(
-            <div className="footer">  
-            <Navbar color="dark" dark>
-                <div>
-                    <NavbarBrand>Footer</NavbarBrand>
-                </div>
-            </Navbar>
-        </div>
+            <div className="footer text-center" id="footer">&copy; 2020 The Custom Corner</div>
         )
     }
 }
