a2bae0c1705af5493c374662a1262f142af1e2b1
Toney Higgins <toneyhiggins2@gmail.com>
2019-12-21 12:47:09 -0500

    adding item card fields to tackboard div

--- client/src/components/TacBoard/index.js
+++ client/src/components/TacBoard/index.js
@@ -1,6 +1,35 @@
 import React from "react";
 import "../../styles/TacBoard.css";
+import "../ItemCard/index";
+
 
 function TackBoard(props) {
-    
+
+    return (
+        <div className="tackboard-container" data-vendor={props.vendor}>
+      <img alt={props.description} src={props.image} />
+      <div className="item-description">
+        <li>Vendor: {props.vendor}</li>
+        <li className="scrollText">
+          <span>Description: {props.description}</span>
+        </li>
+        <li>SKU: {props.sku}</li>
+        <li>
+          URL:{" "}
+          <a href={props.url} target="_blank" rel="noopener noreferrer">
+            Link
+          </a>
+        </li>
+        <li>
+          Tearsheet:{" "}
+          <a href={props.tearsheet} target="_blank" rel="noopener noreferrer">
+            Link
+          </a>
+        </li>
+      </div>
+    </div>
+    )
+
 }
+
+export default TackBoard;
