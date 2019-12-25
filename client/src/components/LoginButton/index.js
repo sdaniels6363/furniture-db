2528a69a3c7dc14cae2c8dfcee0e53c94540240d
JHaarla <haarladesign@gmail.com>
2019-12-24 20:16:59 -0500

    added Login button to About page. Also, some minor CSS tweaks

---
+++ client/src/components/LoginButton/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../../styles/About.css";
+
+
+class LoginButtonAbout extends React.Component {
+    render() {
+        return (
+            <div className="btn-wrapper">
+                <button type="button" id="about-login-btn">LOGIN</button>
+            </div>
+            )
+        }
+    }
+    
+    
+    
+    
+    export default LoginButtonAbout;
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,30 +1,36 @@
 import React, { Component } from "react";
 import AboutPic from "../components/AboutPic";
 import AboutText from "../components/AboutText";
+import LoginButtonAbout from "../components/LoginButton";
 
 class About extends Component {
   render() {
-  return (
-  <div>
-    <div className="container">
-      <div className="row">
-        <div className="col-sm" id="mycol">
-          <AboutPic />
-        </div>
-        <div className="col-sm" id="mycol">
+    return (
+      <div>
+        <div className="container" id="mycontainer">
+          <div className="row">
+            <div className="col-md" id="mycol">
+              <AboutPic />
+            </div>
+            <div className="col-md" id="mycol">
+              <AboutText />
+            </div>
           
-          <AboutText />
+          </div>
+          <div className="row">
+              <div className="col-md" id="mycol">
+                <LoginButtonAbout />
+              </div>
+            </div>
         </div>
+
+
+
+
 
       </div>
-    </div>
-
-
-
-
-
-  </div>
-  )
-}}
+    )
+  }
+}
 
 export default About;
