ac70ffc177b55200be3b317c7e58a88d2ee3b3b0
JHaarla <haarladesign@gmail.com>
2019-12-24 23:25:21 -0500

    added some CSS sugar to the About page

--- client/src/components/AboutText/index.js
+++ client/src/components/AboutText/index.js
@@ -7,10 +7,10 @@ class AboutText extends React.Component {
     render() {
         return (
             <div>
-                <div className="card" id="mycard">
+                <div className="card fade-in" id="mycard">
                     <div className="card-body">
                         <h1 className="card-title" id="mycard-title">About Custom Corner</h1>
-                        <p className="card-text" id="mycard-text">
+                        <p className="card-text fade-in" id="mycard-text">
                             <span className="custom-bold">Custom Corner</span> is a tool built for AR Homes to streamline their interior designers’ workflow. Instead of bouncing to dozens of different sites to pick the right furniture, fabrics and other decorative items, the designer can select everything within <span className="custom-bold">Custom Corner</span>. We’ll keep track of what has been selected for each client and keep tabs on it. The final collection of products can then be displayed on Pinterest for easy sharing with the client.
                         </p>
                     </div>
