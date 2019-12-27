ac343f9ff09d045f9020ea88545eb60e310e05e1
Scott Daniels <sdaniels6363@gmail.com>
2019-12-27 12:44:39 -0500

    added calls for add/list/delete client

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,27 +2,37 @@ import axios from "axios";
 
 export default {
     // Gets furniture by Category
-    getFurnitureByCategory: function(category) {
+    getFurnitureByCategory: function (category) {
         return axios.get("/api/furniture/" + category);
     },
     // Gets furniture by Vendor(s)
-    getFurnitureByVendor: function(category, vendor) {
+    getFurnitureByVendor: function (category, vendor) {
         return axios.get(`/api/furniture/${category}/${vendor}`);
     },
     // Gets all unique Vendors
-    getVendors: function() {
+    getVendors: function () {
         return axios.get(`/api/vendor`);
     },
     // Get all unique Categories
-    getCategories: function(){
+    getCategories: function () {
         return axios.get("/api/categories");
     },
-    validateUser: function(){
+    validateUser: function () {
         return axios.post("/api/auth/login")
     },
-    newUser: function(){
+    newUser: function () {
         return axios.post("/api/auth/create")
+    },
+    newClient: function (data) {
+        console.log(data)
+        return axios.post("/api/clients/create", { data }).then((res, err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log(res)
+        })
+    },
+    getClients: function () {
+        return axios.get("/api/clients/list")
     }
-
-
 };
