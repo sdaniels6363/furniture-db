3a59e254ba333389f94f51038b904b3818615982
Scott Daniels <sdaniels6363@gmail.com>
2019-12-27 15:39:13 -0500

    Delete functionality added, and sort by name added

--- client/src/components/SelectClient/index.js
+++ client/src/components/SelectClient/index.js
@@ -3,51 +3,73 @@ import "../../styles/Clients.css";
 import API from "../../utils/API";
 
 class SelectClient extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            clients: [],
-            selected: ""
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      clients: [],
+      selected: ""
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.deleteClient = this.deleteClient.bind(this);
+  }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
-    }
+  handleChange(event) {
+    this.setState({ selected: event.target.value });
+  }
 
-    handleSubmit(event) {
-        alert('Your favorite flavor is: ' + this.state.value);
-        event.preventDefault();
-    }
+  deleteClient(event) {
+    event.preventDefault();
+    const body = { id: this.state.selected };
+    API.deleteClient(body).then((res, err) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(res);
+    });
+  }
 
-    componentDidMount() {
-        API.getClients().then(res => this.setState({ clients: res.data.sort() }));
-    }
+  componentDidMount() {
+    API.getClients().then(res => {
+      let clients = res.data;
+      console.log(res.data)
+      console.log(clients.sort((a, b) => a.name.localeCompare(b.name)));
 
+      this.setState({
+        clients: clients,
+        selected: res.data[0]._id
+      });
+    });
+  }
 
-
-    render() {
-        return (
-            <div>
-
-                <h4 id="client-card-text" className="top-spacer">Select Client</h4>
-                <div className="form-group">
-                    <select value={this.state.selected} className="form-control" id="exampleFormControlSelect1">
-                        {this.state.clients.map((client) => {
-                            return (
-                                <option data-id={client._id}>{client.name}</option>
-                            )
-                        })}
-                    </select>
-                    <button className="btn btn-danger">Delete Client</button>
-                </div>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h4 id="client-card-text" className="top-spacer">
+          Select Client
+        </h4>
+        <div className="form-group">
+          <select
+            value={this.state.selected}
+            onChange={this.handleChange}
+            className="form-control"
+            id="exampleFormControlSelect1"
+          >
+            {this.state.clients.map(client => {
+              return (
+                <option key={client.name} value={client._id}>
+                  {client.name}
+                </option>
+              );
+            })}
+          </select>
+          <button className="btn btn-danger" onClick={this.deleteClient}>
+            Delete Client
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SelectClient;
