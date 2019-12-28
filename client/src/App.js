e177783f7cf964e96717e18ad1b2e41981a45d7d
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-12-28 10:41:54 -0500

    Added Form component and a login page to pages.  Needs some styling.  Added a separate Login component using Formik that  I'm playing with, but unsuccessful as of now - feel free to tweak around with it please.  It's not connected to anything else, so can delete at the end if not implemented...

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import CatNav from "./components/CatNav"
 import About from "./pages/About";
 import Items from "./pages/Items";
 import Header from "./components/Header";
+import Form from "./components/Form";
 // import Footer from "./components/Footer";
 
 
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
+          <Route exact path="/login" component={Form} />
           <Route exact path="/category/:item" render={(props) => <Items {...props} />} />
         </Switch>
         {/* <Footer /> */}
---
+++ client/src/components/Form/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import "../../styles/Form.css";
+
+
+class loginForm extends React.Component {
+  render() {
+    return (
+      <Container>
+      <Form>
+        <Form.Group controlId="formBasicLogin">
+          {/* <Form.Label>Login</Form.Label> */}
+          <Form.Control type="email" placeholder="Enter Login" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          {/* <Form.Label>Password</Form.Label> */}
+          <Form.Control type="password" placeholder="Enter Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </Container>
+    )
+  }
+}
+
+
+export default loginForm;
