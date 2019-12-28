f61cb4110dd5f714acfd32dc6e134af414e3189c
Scott Daniels <sdaniels6363@gmail.com>
2019-12-28 00:27:43 -0500

    renamed functions for clarity

--- client/src/components/AddClient/index.js
+++ client/src/components/AddClient/index.js
@@ -8,14 +8,14 @@ class AddClient extends React.Component {
         this.state = { newClient: '' };
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.newClient = this.newClient.bind(this);
     }
 
     handleChange(event) {
         this.setState({ newClient: event.target.value });
     }
 
-    handleSubmit(event) {
+    newClient(event) {
         event.preventDefault();
         const body = {name: this.state.newClient}
         API.newClient(body).then((res,err) => {
@@ -36,7 +36,7 @@ class AddClient extends React.Component {
                         <div className="input-group">
                             <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" placeholder="Add a new client" aria-label="Recipient's username" aria-describedby="basic-addon2" />
                             <div className="input-group-append">
-                                <input className="btn btn-outline-secondary" type="submit" value="submit" onClick={this.handleSubmit} />
+                                <input className="btn btn-outline-secondary" type="submit" value="submit" onClick={this.newClient} />
                             </div>
                         </div>
                     </div>
