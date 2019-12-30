8111df287fb13e001ab1b84861c1a147a44667c1
Toney Higgins <toneyhiggins2@gmail.com>
2019-12-30 17:54:04 -0500

    adding tackboard css

--- client/src/components/TacBoard/index.js
+++ client/src/components/TacBoard/index.js
@@ -5,30 +5,33 @@ import "../ItemCard/index";
 
 function TackBoard(props) {
 
-    return (
-        <div className="tackboard-container" data-vendor={props.vendor}>
-      <img alt={props.description} src={props.image} />
-      <div className="tackboard-description">
-        <li>Vendor: {props.vendor}</li>
-        <li className="scrollText">
-          <span>Description: {props.description}</span>
-        </li>
-        <li>SKU: {props.sku}</li>
-        <li>
-          URL:{" "}
-          <a href={props.url} target="_blank" rel="noopener noreferrer">
-            Link
+  return (
+    <div className="tackboard-container1">
+      <div className="tackboard-container2" data-vendor={props.vendor}>
+        <img alt={props.description} src={props.image} />
+        <div className="tackboard-description">
+          <li>Vendor: {props.vendor}</li>
+          <li className="scrollText">
+            <span>Description: {props.description}</span>
+          </li>
+          <li>SKU: {props.sku}</li>
+          <li>
+            URL:{" "}
+            <a href={props.url} target="_blank" rel="noopener noreferrer">
+              Link
           </a>
-        </li>
-        <li>
-          Tearsheet:{" "}
-          <a href={props.tearsheet} target="_blank" rel="noopener noreferrer">
-            Link
+          </li>
+          <li>
+            Tearsheet:{" "}
+            <a href={props.tearsheet} target="_blank" rel="noopener noreferrer">
+              Link
           </a>
-        </li>
+          </li>
+        </div>
       </div>
     </div>
-    )
+
+  )
 
 }
 
