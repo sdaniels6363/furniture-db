7b17a33e2f02415f5d682830c756bc6006287605
JHaarla <haarladesign@gmail.com>
2019-12-30 08:55:32 -0500

    Added hamburger menu to navbar. It houses all other menu/nav items except the categories that are dynamically created. Minor CSS tweaks.

--- client/src/components/CatNav/index.js
+++ client/src/components/CatNav/index.js
@@ -47,28 +47,6 @@ class CatNav extends Component {
       //   </div>
       // </nav>
 
-
-      // <div>
-      //   <div className="collapse admin-controls" id="burger-items">
-      //     <a className="navbar-brand" href="/clients">CLIENTS</a>
-      //     <a className="navbar-brand" href="/about">ABOUT</a>
-      //   </div>
-      //   <nav className="navbar navbar-expand" id="catNavBar">
-      //     <button className="navbar-toggler admin-controls" type="button" data-toggle="collapse" data-target="#burger-items" aria-controls="burger-items" aria-expanded="false" aria-label="Toggle navigation"><span><i class="fas fa-bars fa-1x"></i></span></button>
-
-      //     {this.state.categories.map((category, i) => {
-      //       return (
-      //         <a
-      //           key={i}
-      //           className="navbar-brand"
-      //           href={`/category/${category}`}>
-      //           {category.toUpperCase()}
-      //         </a>
-      //       );
-      //     })}
-      //   </nav>
-      // </div>
-
       <nav className="navbar navbar-expand-lg" id="catNavBar">
 
         {/* All navigation links with the exception of the About page are derived from a database call.
