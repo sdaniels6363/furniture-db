32fd63c2569f0d925d192edee9b6181dd6e2d77a
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2019-12-30 19:43:13 -0500

    Removed Submit button from Form Component as it's now unnecessary

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,16 +12,16 @@ class LoginForm extends React.Component {
         <Form>
           <Form.Group controlId="formBasicLogin">
             {/* <Form.Label>Login</Form.Label> */}
-            <Form.Control type="email" placeholder="Enter Login" />
+            <Form.Control type="text" placeholder="Enter Login" />
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             {/* <Form.Label>Password</Form.Label> */}
             <Form.Control type="password" placeholder="Enter Password" />
           </Form.Group>
-          <Button variant="primary" type="submit">
+          {/* <Button variant="primary" type="submit">
             Submit
-        </Button>
+        </Button> */}
         </Form>
       </Container>
     )
