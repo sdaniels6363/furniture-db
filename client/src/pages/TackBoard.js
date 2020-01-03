6892b7a3227021e5a40a82e24465a87c422574d7
Scott Daniels <sdaniels6363@gmail.com>
2020-01-03 07:46:22 -0500

    Formatting and placeholder Div Informing user they need to select items.

--- client/src/pages/TackBoard.js
+++ client/src/pages/TackBoard.js
@@ -4,58 +4,61 @@ import ItemCard from "../components/ItemCard";
 import API from "../utils/API";
 
 class TackBoard extends Component {
+  state = {
+    clientItems: [],
+    selectedClient: ""
+  };
+  componentDidMount() {
+    this.loadclientList();
+  }
 
-    state = {
-        clientItems: [],
-        selectedClient: ""
-    };
-    componentDidMount() {
-        this.loadclientList()
+  loadclientList = () => {
+    let client = localStorage.getItem("selectedClient");
+
+    if (client === "") {
+      alert("Please Select a Client");
+      return;
     }
+    API.getClientItems({ client: client })
+      .then(res => {
+        this.setState({ clientItems: res.data });
+      })
+      .catch(err => console.log(err));
+  };
 
-    loadclientList = () => {
-        let client = localStorage.getItem("selectedClient");
-        
-        if (client === "") {
-            alert("Please Select a Client");
-            return
-        }
-        API.getClientItems({client: client}).then(res => {
-            this.setState({clientItems: res.data})
-        }).catch(err => console.log(err))
-    }
+  render() {
+    return (
+      // This component will pull in all items selected for the client and create the horizontal cards for display.
 
-    render() {
-        return (
-
-            // This component will pull in all items selected for the client and create the horizontal cards for display.
-
-            <div>
-                <div className="row">
-                    <div className="col-md">
-
-                        <div className="tackboard-container1">
-                            {this.state.clientItems.map((itemList, i) => {
-                                return (
-                                    <ItemCard
-                                        key={i}
-                                        _id={itemList._id}
-                                        vendor={itemList.item.vendor}
-                                        description={itemList.item.description}
-                                        image={itemList.item.image}
-                                        sku={itemList.item.sku}
-                                        url={itemList.item.url}
-                                        tearsheet={itemList.item.tearsheet}
-                                    />
-                                )
-                            })};
-                        </div>
-                    </div>
-                </div>
-
+      <div>
+        <div className="row">
+          <div className="col-md">
+            <div className="tackboard-container1">
+              {this.state.clientItems.length === 0 ? (
+                <h3>Please select some items.</h3> // temp need to add a nicer placeholder
+              ) : (
+                this.state.clientItems.map((itemList, i) => {
+                  return (
+                    <ItemCard
+                      key={i}
+                      _id={itemList._id}
+                      vendor={itemList.item.vendor}
+                      description={itemList.item.description}
+                      image={itemList.item.image}
+                      sku={itemList.item.sku}
+                      url={itemList.item.url}
+                      tearsheet={itemList.item.tearsheet}
+                    />
+                  );
+                })
+              )}
+              ;
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default TackBoard;
