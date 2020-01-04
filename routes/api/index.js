bb7b46d891eb3ee1966f1db82069ce8d19a58703
Scott Daniels <sdaniels6363@gmail.com>
2020-01-04 15:10:30 -0500

    removed category routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,14 +1,12 @@
 const router = require("express").Router();
 const furnitureRoutes = require("./furniture.js");
 const vendorRoutes = require("./vendor");
-const categoryRoutes = require("./category")
 const clientRoutes = require("./clients")
 const authRoutes = require("./auth")
 const roomRoutes = require("./rooms")
 // Furniture and Room routes
 router.use("/furniture", furnitureRoutes);
 router.use("/vendor", vendorRoutes);
-router.use("/categories", categoryRoutes);
 router.use("/clients", clientRoutes)
 router.use("/auth",authRoutes)
 router.use("/rooms", roomRoutes)
