2f8abec20c5e222062451ac58b144f2c3bbf550d
bradleyllew <51133387+bradleyllew@users.noreply.github.com>
2020-01-04 11:30:20 -0500

    Added roomRoutes to api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,11 +4,13 @@ const vendorRoutes = require("./vendor");
 const categoryRoutes = require("./category")
 const clientRoutes = require("./clients")
 const authRoutes = require("./auth")
+const roomRoutes = require("./rooms")
 // Furniture routes
 router.use("/furniture", furnitureRoutes);
 router.use("/vendor", vendorRoutes);
 router.use("/categories", categoryRoutes);
 router.use("/clients", clientRoutes)
 router.use("/auth",authRoutes)
+router.use("/rooms", roomRoutes)
 
 module.exports = router;
