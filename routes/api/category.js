57d2025d6ff18bb9dd6c987cba639ff3498765d3
Scott Daniels <sdaniels6363@gmail.com>
2020-01-04 15:10:18 -0500

    categoryController merged with roomsController

--- routes/api/category.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const categoryController = require("../../controllers/categoryController");
-
-router.route("/")
-  .get(categoryController.findCategories);
-
-
-module.exports = router;
