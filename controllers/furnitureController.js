3c24fd212680708fc56bd5a9186824e60a205370
Scott Daniels <sdaniels6363@gmail.com>
2020-01-04 16:27:31 -0500

    added room to the query

--- controllers/furnitureController.js
+++ controllers/furnitureController.js
@@ -3,9 +3,10 @@ const db = require("../models");
 // Defining methods for the furnitureController
 module.exports = {
   findByCategory: function(req, res) {
+    let room = req.params.room
     let category = req.params.category;
     db.Furniture
-      .find({category: category})
+      .find({roomName: room ,category: category})
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
