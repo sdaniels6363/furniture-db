5cf501e3013c869e40539b704dec0e0877875d29
Scott Daniels <sdaniels6363@gmail.com>
2020-01-06 16:43:18 -0500

    deleted excess shit

--- scripts/models/index.js
+++
@@ -1,8 +0,0 @@
-// Exporting an object containing all of our models
-module.exports = {
-  Client: require("./Client"),
-  Furniture: require("./Furniture"),
-  User: require("./User"),
-  Tackboard: require("./Tackboard"),
-  // ConnectionLog: require("./ConnectionLog")
-};
